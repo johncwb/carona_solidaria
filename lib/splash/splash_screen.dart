a842d95248ce1137708d5331fbfbf497b16333ab
jpthimotheo <jpthimotheoo@hotmail.com>
2023-06-15 15:54:12 -0300

    arrumado bug do manifest

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -4,7 +4,6 @@ import 'dart:async';
 import 'package:carona_solidaria/register/registerScreen.dart';
 import 'package:carona_solidaria/widgets/auth_check.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -24,64 +23,38 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Timer(const Duration(seconds: 3), () {
       final user = FirebaseAuth.instance.currentUser!;
+
+      // ignore: unnecessary_null_comparison
       if (user != null) {
-        debugPrint(user.displayName);
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: ((context) => const AuthCheck()),
-          ),
-        );
+        debugPrint(user.uid);
+
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: ((context) => const AuthCheck()),
+        //   ),
+        // );
       } else {
         debugPrint("Precisa do login!");
       }
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: ((context) => const RegisterScreen()),
-        ),
-      );
+      // Navigator.push(
+      //   context,
+      //   MaterialPageRoute(
+      //     builder: ((context) => const RegisterScreen()),
+      //   ),
+      // );
     });
-
-    // listener = InternetConnectionChecker().onStatusChange.listen(
-    //   (event) {
-    //     final notifier = ConnectionNotifier.of(context);
-    //     notifier.value =
-    //         event == InternetConnectionStatus.connected ? true : false;
-    //     String? connection = notifier.value ? "Conectado" : "Desconectado";
-    //     debugPrint("O dispositivo está $connection");
-    //     if (notifier.value) {
-    //       Navigator.push(
-    //         context,
-    //         MaterialPageRoute(
-    //           builder: ((context) => const HomeScreen()),
-    //         ),
-    //       );
-    //     } else {
-    //       Navigator.push(
-    //         context,
-    //         MaterialPageRoute(
-    //           builder: ((context) => const NoConnection()),
-    //         ),
-    //       );
-    //     }
-    //   },
-    // );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: const Color(0xff113d63),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             _buildIconRow(),
             const SizedBox(height: 20),
-            // _buildTitle(),
-            // const SizedBox(height: 15),
-            // _buildSubtitle(),
           ],
         ),
       ),
@@ -99,23 +72,23 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
-  Widget _buildTitle() {
-    return Text(
-      "Carona Solidária",
-      style: GoogleFonts.anton(
-        color: const Color(0xff113d63),
-        fontSize: 42,
-      ),
-    );
-  }
+  // Widget _buildTitle() {
+  //   return Text(
+  //     "Carona Solidária",
+  //     style: GoogleFonts.anton(
+  //       color: const Color(0xff113d63),
+  //       fontSize: 42,
+  //     ),
+  //   );
+  // }
 
-  Widget _buildSubtitle() {
-    return Text(
-      "Universidade Tuiuti do Paraná",
-      style: GoogleFonts.anton(
-        color: const Color(0xff113d63),
-        fontWeight: FontWeight.w300,
-      ),
-    );
-  }
+  // Widget _buildSubtitle() {
+  //   return Text(
+  //     "Universidade Tuiuti do Paraná",
+  //     style: GoogleFonts.anton(
+  //       color: const Color(0xff113d63),
+  //       fontWeight: FontWeight.w300,
+  //     ),
+  //   );
+  // }
 }
